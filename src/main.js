22aff39712da84df5a0e43eeaf018a80d024305a
glen <glendymarmatute@gmail.com>
2023-02-03 19:05:01 -0500

    Prueba en vivo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
+const name = "prueba";
 console.log(example, data);
-console.log("Test");
+console.log(name);
